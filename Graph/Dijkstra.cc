ff52f1396a9a9c583fcf57644e388e80abd89802
SakaTaku-lib <ascii2824@gmail.com>
2020-07-03 05:10:29 +0900

    refine Dijkstra

--- Graph/Dijkstra.cc
+++ Graph/Dijkstra.cc
@@ -1,80 +1,37 @@
+//2020-07-03 04:37:43
+//writer SakaTaku
+//Dijkstra法   O(|E|log|V|)
+using ll = long long;
 #include<bits/stdc++.h>
 using namespace std;
-using ll = long long;
-struct WeightedGraph
-{
-   int n; //頂点は[0:n)
-   WeightedGraph(int n):n(n){}
-   struct Edge {
-      int ep;  //ep:EndPoint
-      ll  w;   //w:Weight
-      Edge(int ep, ll w):ep(ep),w(w){}
-   };
-   vector<vector<Edge>> E;
-   void add(int v, int w, ll weight){
-      E[v].push_back(Edge(w, weight));
+struct Edge {int to; ll cost;};
+typedef pair<ll, int> P;
+int V;
+struct Dijkstra {
+   int V;
+   vector<vector<Edge>> G;
+   Dijkstra(int V):G(V){}
+   void add(int v, int w, int cost){
+      Edge e;
+      e.to = w; e.cost = cost;
+      G[v].push_back(e);
    }
-};
-
-struct Dijkstra
-{
-   int s;
-   WeightedGraph wg;
+   ll inf = (1<<60);
    vector<ll> d;
-   Dijkstra(WeightedGraph wg, int s=0):wg(wg),s(s){
-      d = vector<ll>(wg.n);
-   };
-
-};
-
-void dijkstra(int x){
-    int minv;
-    int d[MAX], color[MAX];
-    for (int i = 0; i < n; i++)
-    {
-        d[i] = INF;
-        color[i] = WHITE;
-    }
-    d[x] = 0;
-    color[x] = GRAY;
-    while (true)
-    {
-        minv = INF;
-        int u = -1;
-        for (int i = 0; i < n; i++){
-            if (minv > d[i] && color[i] != BLACK){
-                u = i;
-                minv = d[i];
+   void solve(int s){
+      priority_queue<P, vector<P>, greater<P>> pq;
+      d = vector<ll>(V,inf);
+      d[s] = 0;
+      pq.push(P(0,s));
+      while (!pq.empty()){
+         P p = pq.top(); pq.pop();
+         if (d[p.second] <p.first)continue;
+         for (auto &&e : G[p.second]){
+            if (d[e.to] > d[p.second] + e.cost){
+               d[e.to] = d[p.second] + e.cost;
+               pq.push(P(d[e.to], e.to));
             }
-        }
-        if (u == -1) break;
-        color[u] = BLACK;
-        for (int v = 0; v < n; v++)
-            if (color[v] != BLACK && M[u][v] != INF)
-                if (d[v] > d[u] + M[u][v])
-                {
-                    d[v] = d[u] + M[u][v];
-                    color[v] = GRAY;
-                }
-    }
-    if (d[0] == INF)
-        {
-            cout << "INF";
-        }
-        else
-        {
-            cout << d[0];
-        }
-    for (int i = 1; i < n; i++)
-    {
-        if (d[i] == INF)
-        {
-            cout << " INF";
-        }
-        else
-        {
-            cout << " " << d[i];
-        }
-        
-    }
-}
+         }  
+      }
+   }
+}; 
